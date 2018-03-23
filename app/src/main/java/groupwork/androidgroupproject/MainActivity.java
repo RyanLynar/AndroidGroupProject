3fd1606d99ae76e7efbff75f71e00f9512414df6
RyanLynar <lyna0001@algonquinlive.com>
2018-03-23 15:29:31 -0400

    Started my Branch in theory

--- app/src/main/java/groupwork/androidgroupproject/MainActivity.java
+++ app/src/main/java/groupwork/androidgroupproject/MainActivity.java
@@ -2,12 +2,19 @@ package groupwork.androidgroupproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button movieBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        movieBtn = findViewById(R.id.movieLaunch);
+        movieBtn.setOnClickListener(t-> {
+            Log.i("DEBUGINGTEXT","Clicekd MovieBtn");
+        });
+
     }
 }
