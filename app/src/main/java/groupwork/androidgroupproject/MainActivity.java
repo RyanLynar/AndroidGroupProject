66e6fa30dff55f3468cfdbe306d9691a3cdc1d42
RyanLynar <lyna0001@algonquinlive.com>
2018-03-28 19:58:23 -0400

    Imported Stefan's stuff, broke Ryan's

--- app/src/main/java/groupwork/androidgroupproject/MainActivity.java
+++ app/src/main/java/groupwork/androidgroupproject/MainActivity.java
@@ -2,16 +2,9 @@ package groupwork.androidgroupproject;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.Context;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.ProgressBar;
-import android.util.Log;
-import android.widget.Button;
 
 public class MainActivity extends Activity {
     private ProgressBar progress;
@@ -33,16 +26,15 @@ public class MainActivity extends Activity {
         });
         movieBtn = findViewById(R.id.movieLaunch);
         movieBtn.setOnClickListener(t-> {
-            Intent in = new Intent(this,MovieDBActivity.class);
-            startActivity(in);
+            //Intent in = new Intent(this,MovieDBActivity.class);
+            //startActivity(in);
         });
-        Button patientButton = (Button) findViewById(R.id.patientButton);
-        patientButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        Button patientButton =  findViewById(R.id.patientButton);
+        patientButton.setOnClickListener(t -> {
+
                 Intent intent = new Intent(MainActivity.this, ListItemsActivity.class);
                 startActivityForResult(intent, 5);
-            }
+
         });
     }
 }
