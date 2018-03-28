54373adef11357548b3c0cb5492f15305029e17b
alav0006 <alav0006@algonquinlive.com>
2018-03-28 19:26:47 -0400

    milestone 1

--- app/src/main/java/groupwork/androidgroupproject/MainActivity.java
+++ app/src/main/java/groupwork/androidgroupproject/MainActivity.java
@@ -1,13 +1,29 @@
 package groupwork.androidgroupproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    private ProgressBar progress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progress = (ProgressBar)findViewById(R.id.progressBar);
+        progress.setVisibility(View.VISIBLE);
+
+        Button patientButton = (Button) findViewById(R.id.patientButton);
+        patientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListItemsActivity.class);
+                startActivityForResult(intent, 5);
+            }
+        });
     }
 }
