cb4f91ae2b32d3fcb8c450574417b866d3a60196
RyanLynar <lyna0001@algonquinlive.com>
2018-03-29 11:50:30 -0400

    Squashed commit of the following:
    commit 3d1ab06bec0a88f80251ff1f0b4d9106b98107d6
    Author: Adam Andrews <andr0272@algonquinlive.com>
    Date:   Wed Mar 28 21:15:35 2018 -0400
    
        initial commit (Multiple choice section)
    
    commit 5aa4e532ba4ed88315e94fb82fdfc19e6bd4692d
    Author: Adam Andrews <andr0272@algonquinlive.com>
    Date:   Wed Mar 28 21:03:28 2018 -0400
    
        initial commit (Multiple choice section)

--- app/src/main/java/groupwork/androidgroupproject/ListItemsActivity.java
+++ app/src/main/java/groupwork/androidgroupproject/ListItemsActivity.java
@@ -3,7 +3,6 @@ package groupwork.androidgroupproject;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,16 +13,38 @@ public class ListItemsActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ListView listView = (ListView) findViewById(R.id.listView);
+       //    ListView listView =  findViewById(R.id.listView);
 
         String[] patientInfo = new String[] { "Name", "Address", "Birthday", "Phone Number",
                 "Health Card Number", "Description of visit"};
         ArrayList<String> PatientList = new ArrayList<String>();
         PatientList.addAll( Arrays.asList(patientInfo) );
 
-        listAdapter = new ArrayAdapter<String>(this, R.layout.list_items,PatientList);
+           /* Button toastBtn = findViewById(R.id.someBtn);
 
-        listView.setAdapter(listAdapter);
+            toastBtn.setOnClickListener((View t) -> {
+                Toast toast = Toast.makeText(getApplicationContext(), "One Day I dream of doing something", Toast.LENGTH_SHORT);
+                toast.setMargin(50, 50);
+                toast.show();
+                Notification customNotification = new NotificationCompat.Builder(getApplicationContext(), "0")
+                        .setSmallIcon(R.drawable.ic_launcher_foreground)
+                        .setStyle(new NotificationCompat.DecoratedCustomViewStyle())
+                        .setContentText("But not yet")
+                        .build();
+                NotificationManager m = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                try {
+                    m.notify("", 0, customNotification);
+                } catch (NullPointerException e) {
 
-    }}
+                }
+
+                Snackbar sb = Snackbar.make(t, "Hello World", Snackbar.LENGTH_SHORT);
+                sb.show();
+            });
+*/
+        //listAdapter = new ArrayAdapter<String>(this, R.layout.list_items,PatientList);
+
+        //listView.setAdapter(listAdapter);
+
+    }
+}
--- app/src/main/java/groupwork/androidgroupproject/MultipleChoice.java
+++ app/src/main/java/groupwork/androidgroupproject/MultipleChoice.java
@@ -1,10 +1,16 @@
 package groupwork.androidgroupproject;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by Adam on 2018-03-28.
@@ -27,11 +33,25 @@ public class MultipleChoice extends Activity {
         existingQuiz=(Button)findViewById(R.id.ExistingQuizButton);
 
         newQuiz.setOnClickListener(t->{
-            Log.i(ACTIVITY_NAME,"User clicked 'new quiz'");
+                Toast toast =  Toast.makeText(getApplicationContext(),"One Day I dream of doing something",Toast.LENGTH_SHORT );
+                toast.setMargin(50,50);
+                toast.show();
+                Notification customNotification = new NotificationCompat.Builder(getApplicationContext(),"0")
+                        .setSmallIcon(R.drawable.ic_launcher_foreground)
+                        .setStyle(new NotificationCompat.DecoratedCustomViewStyle())
+                        .setContentText("But not yet")
+                        .build();
+                NotificationManager m = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                try{
+                    m.notify("",0,customNotification);
+                }catch(NullPointerException e){
 
-            intent = new Intent(MultipleChoice.this, NewQuiz.class);
-            startActivity(intent);
-        });
+                }
+
+                Snackbar sb = Snackbar.make(t,"Hello World",Snackbar.LENGTH_SHORT);
+                sb.show();
+            });
+
 
         existingQuiz.setOnClickListener(t->{
             Log.i(ACTIVITY_NAME,"User clicked 'existing quiz'");
